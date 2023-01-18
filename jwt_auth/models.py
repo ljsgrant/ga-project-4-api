4144e39b19a8f0bdc1991906a96cc56cbba0a012
Louis_Grant <ljsgrant@gmail.com>
2023-01-18 15:29:29 +0000

    create common user serializer

--- jwt_auth/models.py
+++ jwt_auth/models.py
@@ -9,7 +9,7 @@ class User(AbstractUser):
     # posts = models.ManyToManyField('posts.Post', related_name='post_owner')
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    profile_image = models.CharField(max_length=300)
+    profile_picture = models.CharField(max_length=300)
     # photos = models.ManyToManyField('photos.Photo', related_name='photo_owner')
 
     def __str__(self):
