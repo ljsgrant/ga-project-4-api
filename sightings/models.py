e1f5857acb6517f0a5e8fb5a4507d195d076b2d8
Louis_Grant <ljsgrant@gmail.com>
2023-01-23 12:59:48 +0000

    add image field to sighting

--- sightings/models.py
+++ sightings/models.py
@@ -2,13 +2,15 @@ from django.db import models
 
 
 class Sighting(models.Model):
-    bird_sighted = models.ForeignKey("birds.Bird", related_name="sightings", on_delete=models.CASCADE)
+    bird_sighted = models.ForeignKey(
+        "birds.Bird", related_name="sightings", on_delete=models.CASCADE)
     sighted_at_datetime = models.DateTimeField()
     location_lat = models.FloatField(max_length=10)
     location_long = models.FloatField(max_length=10)
     notes = models.CharField(max_length=500)
     owner = models.ForeignKey(
         "jwt_auth.User", related_name="sightings", on_delete=models.CASCADE)
+    image = models.CharField(max_length=40)
     posted_on_timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
