bf6a716263eea4c4f359423dfba60e890b4a80ca
Louis_Grant <ljsgrant@gmail.com>
2023-01-26 23:08:32 +0000

    deployment: add cors allowed origins

--- project/settings.py
+++ project/settings.py
@@ -57,8 +57,8 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "https://example.com",
-    "https://sub.example.com",
+    "https://birdspotter.netlify.app",
+    # "https://sub.example.com",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
 ]
